5616e2d983536045e060e2708ca68a1dabc3b434
light-max <3045033935@qq.com>
2023-10-16 14:37:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lifengqiang/videoeditor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lifengqiang.videoeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
